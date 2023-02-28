16c45fb658f72ccf651f4e213e4f42d2a79e560d
Lucas Michon <wagashisakatsuki@gmail.com>
2023-02-28 14:42:14 +0100

    feat: basic athentication

--- src/components/forms/SignUp/SignUp.tsx
+++ src/components/forms/SignUp/SignUp.tsx
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
+import {auth} from '../../../config/firebase'
+import {createUserWithEmailAndPassword} from 'firebase/auth'
 import {Button, Label, TextInput, Modal} from "flowbite-react"
 import Login from "../Login/Login"
 
 function SignUp() {
 
     const [showModal, setShowModal] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
     
     // @ts-ignore
     return (
@@ -22,53 +39,86 @@ function SignUp() {
                 <Modal.Body>
                     <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
                         <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-                            Sign in to our platform
+                            Créer son Compte
                         </h3>
-                        <div>
+                        {/* <div>
                             <div className="mb-2 block">
                                 <Label
-                                    htmlFor="email"
-                                    value="Your email"
+                                    htmlFor="first-name"
+                                    value="Prénom"
                                 />
                             </div>
                             <TextInput
-                                id="email"
-                                placeholder="name@company.com"
+                                id="first-name"
+                                placeholder="Thierry"
                                 required={true}
                             />
                         </div>
                         <div>
                             <div className="mb-2 block">
                                 <Label
-                                    htmlFor="password"
-                                    value="Your password"
+                                    htmlFor="last-name"
+                                    value="Nom"
                                 />
                             </div>
                             <TextInput
-                                id="password"
-                                type="password"
+                                id="last-name"
+                                placeholder="Chevalier"
                                 required={true}
                             />
-                        </div>
-                        <div className="flex justify-between">
-                            <div className="flex items-center gap-2">
-                                {/*<No Display Name id="remember" />*/}
-                                <Label htmlFor="remember">
-                                    Remember me
-                                </Label>
+                        </div> */}
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label
+                                        htmlFor="email"
+                                        value="Adresse Mail"
+                                    />
+                                </div>
+                                <TextInput
+                                    id="email"
+                                    placeholder="name@company.com"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required={true}
+                                />
                             </div>
-                            <a
-                                href="/modal"
-                                className="text-sm text-blue-700 hover:underline dark:text-blue-500"
-                            >
-                                Lost Password?
-                            </a>
-                        </div>
-                        <div className="w-full">
-                            <Button>
-                                Log in to your account
-                            </Button>
-                        </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label
+                                        htmlFor="password"
+                                        value="Mot de Passe"
+                                    />
+                                </div>
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    placeholder="••••••••"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required={true}
+                                />
+                            </div>
+                            <div className="flex justify-between">
+                                <div className="flex items-center gap-2">
+                                    {/*<No Display Name id="remember" />*/}
+                                    <Label htmlFor="remember">
+                                        Remember me
+                                    </Label>
+                                </div>
+                                <a
+                                    href="/modal"
+                                    className="text-sm text-blue-700 hover:underline dark:text-blue-500"
+                                >
+                                    Lost Password?
+                                </a>
+                            </div>
+                            <div className="w-full">
+                                <Button type='submit'>
+                                    Log in to your account
+                                </Button>
+                            </div>
+                        </form>
                         <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
                             Not registered?
                             <a
