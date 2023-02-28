11b55c4375c3fcd4573f5db67b8ac3d62a4f93d4
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-02-28 12:34:56 +0100

    feat: wip adding sign in/up

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,37 @@
+import {Navbar, Button} from "flowbite-react";
+
 const Header = () => {
     return (
         <header>
-            Header
+            <Navbar
+                fluid={true}
+                rounded={true}
+            >
+                <Navbar.Toggle/>
+                <Navbar.Collapse>
+                    <Navbar.Link
+                        href="/navbars"
+                        active={true}
+                    >
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link href="/login">
+                        Tableau de bord
+                    </Navbar.Link>
+                    <div className="flex md:order-2">
+                        <Button>
+                            S'inscrire
+                        </Button>
+                        <Navbar.Toggle />
+                    </div>
+                    <Navbar.Link href="/login">
+                        Se connecter
+                    </Navbar.Link>
+                    <Navbar.Link href="/cart">
+                        Panier
+                    </Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
         </header>
     )
 }
