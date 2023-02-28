f3f6a5e39d5023050508b0d03b8ffe5586137cbc
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-02-28 10:33:29 +0100

    basic setup

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import 'flowbite';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    },
+    // {
+    //     path: "/back",
+    //     element: < />,
+    // },
+    // {
+    //     path: "/about-us",
+    //     element: < />,
+    // }
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
+)
