dd0df23fa82e384a65f0cc1856663654b409cf25
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-03-01 14:41:45 +0100

    fix: merge conflict

--- front/pages/Cart.tsx
+++ front/pages/Cart.tsx
@@ -4,17 +4,19 @@ import Header from "../../src/components/Header/Header";
 
 
 const Cart = () => {
-    const {cart} = useContext(StoreContext)
-    let totalPrice = 0
-    cart[0].forEach(item => {
-        totalPrice += item.item.price
-    })
+    const store = useContext(StoreContext);
+    let totalPrice = 0;
+    if (store && store.cart && store.cart[0]) {
+        store.cart[0].forEach(item => {
+            totalPrice += item.item.price;
+        });
+    }
 
     return (
         <>
             <Header/>
             <div className="container mx-auto px-4">
-                {cart[0] && cart[0][0] ?
+                {store && store.cart && store.cart[0] && store.cart[0][0] ?
                     ` Le prix total de votre panier est de ${totalPrice}€:`
                     : "Le panier est vide."}
             </div>
