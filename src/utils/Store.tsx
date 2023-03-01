851782609ba4e66a19c1687842a43498c22699f9
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-03-01 14:33:18 +0100

    refcctor: type + comment

--- src/utils/Store.tsx
+++ src/utils/Store.tsx
@@ -1,15 +1,30 @@
+// global store
+
 import React, {useState, createContext} from "react"
 
-export const StoreContext = createContext(null)
+interface Props {
+    children: React.ReactNode
+}
 
-export default ({children}) => {
+interface Store {
+    cart: [any[], React.Dispatch<React.SetStateAction<any[]>>],
+    userFireStore: [any, React.Dispatch<React.SetStateAction<any>>],
+    userECom: [any, React.Dispatch<React.SetStateAction<any>>],
+    items: [any[], React.Dispatch<React.SetStateAction<any[]>>],
+    orders: [any[], React.Dispatch<React.SetStateAction<any[]>>]
+}
+
+
+export const StoreContext = createContext<Store | null>(null)
+
+export default ({children}: Props) => {
     const [userFireStore, setUserFireStore] = useState(null),
         [userECom, setUserECom] = useState(null),
-        [items, setItems] = useState(null),
-        [cart, setCart] = useState([]),
-        [orders, setOrders] = useState(null);
+        [items, setItems] = useState<any[]>([]),
+        [cart, setCart] = useState<any[]>([]),
+        [orders, setOrders] = useState<any[]>([]);
 
-    const store = {
+    const store: Store | null = {
         cart: [cart, setCart],
         userFireStore: [userFireStore, setUserFireStore],
         userECom: [userECom, setUserECom],
