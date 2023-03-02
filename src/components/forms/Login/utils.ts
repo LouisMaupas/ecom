e2c7eebdd6da872f4fd86ef992aa8e677aa88b7e
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-03-02 13:34:36 +0100

    refactor: typescript

--- src/components/forms/Login/utils.ts
+++ src/components/forms/Login/utils.ts
@@ -1,7 +1,7 @@
 import {doc, getDoc, collection} from "firebase/firestore";
 import {db} from "../../../config/firebase";
 
-export const getEComUserFromFireBaseUser = async (fireBaseUserId) => {
+export const getEComUserFromFireBaseUser = async (fireBaseUserId: string) => {
     const docRef = doc(db, "user", fireBaseUserId);
     const docSnap = await getDoc(docRef);
     return docSnap.data()
