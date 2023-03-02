174b0163bfa146accea2870a3eb7d6fd51573f66
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-03-02 10:06:58 +0100

    fix: ui

--- front/pages/Cart.tsx
+++ front/pages/Cart.tsx
@@ -76,14 +76,16 @@ const Cart = () => {
                                 </span>
                                 <div>
 
-                                    <Button onClick={(ev) => alert("TODO")}
+                                    <Button size={"xs"}
+                                            onClick={(ev) => alert("TODO")}
                                             href="#"
                                             className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                     >
                                         <span className={"text-white"}>➕</span>
 
                                     </Button>
-                                    <Button onClick={(ev) => alert("TODO")}
+                                    <Button size={"xs"}
+                                            onClick={(ev) => alert("TODO")}
                                             href="#"
                                             className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                     >
