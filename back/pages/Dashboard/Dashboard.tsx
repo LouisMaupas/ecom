784a343adef7b110c286e275b29d9d643a004cc5
Lucas Michon <wagashisakatsuki@gmail.com>
2023-03-02 14:14:25 +0100

    feat: add the list of anonymous orders and restricted access to the dashboard

--- back/pages/Dashboard/Dashboard.tsx
+++ back/pages/Dashboard/Dashboard.tsx
@@ -7,14 +7,28 @@ import {StoreContext} from "../../../src/utils/Store";
 import moment from "moment";
 
 
+interface Item {
+    name: string;
+    quantity: number;
+}
+
+interface User {
+    first_name: string;
+    last_name: string;
+    address: string;
+    email: string;
+    isAdmin : boolean;
+    id: string;
+}
+
 interface Order {
     address: string;
     date: Timestamp;
     email: string;
-    isValidated: boolean;
-    item: string;
+    validated: boolean;
+    item: Array<Item>;
     price: number;
-    user: string;
+    user: User | null;
     id: string;
 }
 
@@ -24,32 +38,52 @@ const Dashboard: React.FunctionComponent = () => {
     const [orders, setOrders] = useState<Order[]>([]);
 
     const fetchOrders = async (): Promise<Order[]> => {
-        const querySnapshot = await getDocs(collection(db, "order"));
-        const OrderAPI: Order[] = [];
-        querySnapshot.forEach((doc) => {
-            const data = doc.data() as Order;
-            OrderAPI.push({
-                id: doc.id,
-                address: data.address,
-                date: data.date,
-                email: data.email,
-                isValidated: data.isValidated,
-                item: data.item,
-                price: data.price,
-                user: data.user,
-            });
+        const [orderSnapshot, userSnapshot] = await Promise.all([
+          getDocs(collection(db, "order")),
+          getDocs(collection(db, "user"))
+        ]);
+      
+        const usersById: Record<string, User> = {};
+        userSnapshot.forEach((doc) => {
+          const user = doc.data() as User;
+          usersById[doc.id] = user;
         });
-        return OrderAPI;
-    }
+      
+        const orders: Order[] = [];
+        orderSnapshot.forEach((doc) => {
+          const order = doc.data() as Order;
+          const user = order.user ? usersById[order.user.id] : null;
+      
+          orders.push({
+            id: doc.id,
+            address: order.address,
+            date: order.date,
+            email: order.email,
+            validated: order.validated,
+            item: order.item,
+            price: order.price,
+            user: user ? {
+              first_name: user.first_name,
+              last_name: user.last_name,
+              address: user.address,
+              email: user.email,
+              id: user.id,
+              isAdmin: user.isAdmin,
+            } : null,
+          });
+        });
+      
+        return orders;
+      };
 
     // Function to validate the order
     const validateOrder = async (orderId: string) => {
         const orderRef = doc(db, "order", orderId.toString());
         await updateDoc(orderRef, {
-          isValidated: true,
+          validated: true,
         });
         const updatedOrders = orders.map((order) =>
-          order.id === orderId ? { ...order, isValidated: true } : order
+          order.id === orderId ? { ...order, validated: true } : order
         );
         setOrders(updatedOrders);
     };
@@ -72,77 +106,84 @@ const Dashboard: React.FunctionComponent = () => {
         });
     }, []);
 
-    console.log(orders);
-
-    return(
-        <div>
-            <Header/>
-            <h4 className="mt-10 mb-5 text-left">Historique des Commandes</h4>
-            <Table>
-                <Table.Head className='text-center'>
-                <Table.HeadCell>
-                    Id Commande
-                </Table.HeadCell>
-                <Table.HeadCell>
-                    Date
-                </Table.HeadCell>
-                <Table.HeadCell>
-                    Adresse Mail
-                </Table.HeadCell>
-                <Table.HeadCell>
-                    Adresse
-                </Table.HeadCell>
-                <Table.HeadCell>
-                    Prix
-                </Table.HeadCell>
-                <Table.HeadCell>
-                    Statut
-                </Table.HeadCell>
-                <Table.HeadCell>
-                    <span className="sr-only">
-                    Supprimer
-                    </span>
-                    Action
-                </Table.HeadCell>
-                </Table.Head>
-                <Table.Body className="divide-y">
-                    {orders.map((order, i) => (
-                        <Table.Row key={i} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                                {order.id}
-                            </Table.Cell>
-                            <Table.Cell>
-                                {moment(order.date.toDate()).format("DD/MM/YYYY")}
-                            </Table.Cell>
-                            <Table.Cell>
-                                {order.email}
-                            </Table.Cell>
-                            <Table.Cell>
-                                {order.address}
-                            </Table.Cell>
-                            <Table.Cell>
-                                {order.price.toString()}.00€
-                            </Table.Cell>
-                            <Table.Cell >
-                                {order.isValidated ? 
-                                <p className='text-green-500'><b>Commande Validé</b></p> : 
-                                <Button onClick={() => validateOrder(order.id)}>
-                                    Valider
-                                </Button>}
-                            </Table.Cell>
-                            <Table.Cell>
-                                <Button onClick={() => deleteOrder(order.id)}>
-                                    Supprimer
-                                </Button>
-                            </Table.Cell>
-                        </Table.Row>
-                    ))}
-
-                </Table.Body>
-            </Table>
-        </div>
- 
-    )
+    if (store?.userFireStore[0]?.email === "admin@admin.com") {
+        return(
+            <div>
+                <Header/>
+                <h4 className="mt-10 mb-5 text-left">Historique des Commandes</h4>
+                <Table>
+                    <Table.Head className='text-center'>
+                    <Table.HeadCell>
+                        Id Commande
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Date
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Nom Complet
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Adresse
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Prix
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Statut
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        <span className="sr-only">
+                        Supprimer
+                        </span>
+                        Action
+                    </Table.HeadCell>
+                    </Table.Head>
+                    <Table.Body className="divide-y">
+                        {orders.map((order, i) => (
+                            <Table.Row key={i} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                    {order.id}
+                                </Table.Cell>
+                                <Table.Cell>
+                                    {moment(order.date.toDate()).format("DD/MM/YYYY")}
+                                </Table.Cell>
+                                <Table.Cell className='text-center'>
+                                    {order.user ? `${order.user.first_name} ${order.user.last_name}`  : "Anonyme"}
+                                </Table.Cell>
+                                <Table.Cell>
+                                    {order.user ? order.user.address : order.address}
+                                </Table.Cell>
+                                <Table.Cell>
+                                    {order.price.toString()}.00€
+                                </Table.Cell>
+                                <Table.Cell >
+                                    {order.validated ? 
+                                    <p className='text-green-500'><b>Commande Validé</b></p> : 
+                                    <Button onClick={() => validateOrder(order.id)}>
+                                        Valider
+                                    </Button>}
+                                </Table.Cell>
+                                <Table.Cell>
+                                    <Button onClick={() => deleteOrder(order.id)}>
+                                        Supprimer
+                                    </Button>
+                                </Table.Cell>
+                            </Table.Row>
+                        ))}
+    
+                    </Table.Body>
+                </Table>
+            </div>
+     
+        )
+    } else {
+        return(
+            <div>
+                <Header/>
+                <p>Vous n'avez pas la permission d'accéder à ce contenu</p>
+            </div>
+        )
+    }
 }
 
 export default Dashboard;
